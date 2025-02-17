68ba98331fb8a6cf7e87d864df56e7505b2c3d32
Gabriel dos Santos <ogabrielmclmg@gmail.com>
2025-02-17 13:29:38 -0300

    exercises

---
+++ 002.js
@@ -0,0 +1,9 @@
+/*
+Create a constant called PI and assign it the value π (3.14). Calculate the area of ​​a circle with radius 5 using this constant and display the result.
+*/
+
+const pi = 3.14
+let radius = 5
+let area = pi * radius * radius
+
+console.log(`area of the circle: ${area}`)
