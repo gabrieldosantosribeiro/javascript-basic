3843e34db6a24dc77597194ececa21bc6f68fb09
Gabriel dos Santos <ogabrielmclmg@gmail.com>
2025-02-17 13:26:54 -0300

    exercises

---
+++ 001.js
@@ -0,0 +1,8 @@
+/*
+Declare a variable called age and assign it an integer value. Then create another variable called name and assign it your name. Print both values ​​to the console.
+*/
+
+let age = 18
+let name = 'gabriel'
+
+console.log(`Age: ${age}, Name: ${name}`)
