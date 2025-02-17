83f3843f2a2537e1e75410beb4d2edef774ab576
Gabriel dos Santos <ogabrielmclmg@gmail.com>
2025-02-17 13:37:13 -0300

    exercises

---
+++ 003.js
@@ -0,0 +1,9 @@
+/*
+Create two variables, a and b, and assign numeric values ​​to them. Use arithmetic operators to calculate addition, subtraction, multiplication, and division, displaying each result.
+*/
+
+let a = 10, b = 5
+console.log(`sum: ${a + b}`)
+console.log(`subtraction: ${a - b}`)
+console.log(`multiplication: ${a * b}`)
+console.log(`division: ${a / b}`)
