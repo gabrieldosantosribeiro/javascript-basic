1ff350ec827fcca1d03cb19087379df106d723dd
Gabriel dos Santos <ogabrielmclmg@gmail.com>
2025-02-18 12:38:11 -0300

    exercises

---
+++ 005.js
@@ -0,0 +1,7 @@
+/*
+Create a variable called sentence and assign it any sentence. Use string methods to count how many words there are in the sentence and display the result.
+*/
+
+let sentence = 'this is a sentence'
+let words = sentence.split(' ')
+console.log(`number of words: ${words.length}`)
