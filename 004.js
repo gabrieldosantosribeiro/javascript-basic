0a7ee9457051fe0054f33c5d1ff85fbd757379d7
Gabriel dos Santos <ogabrielmclmg@gmail.com>
2025-02-18 11:58:44 -0300

    exercises

---
+++ 004.js
@@ -0,0 +1,10 @@
+/*
+Create a variable called num and assign it a number. Use increment and decrement operators to change the value of num and display the value before and after.
+*/
+
+let num = 10
+console.log(`original value: ${num}`)
+num++
+console.log(`after increment: ${num}`)
+num--
+console.log(`after decrement: ${num}`)
