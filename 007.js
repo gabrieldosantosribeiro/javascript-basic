2425ff2ffee47f14513fc1b955666b6d16d55c15
Gabriel dos Santos <ogabrielmclmg@gmail.com>
2025-02-19 14:39:05 -0300

    exercises

---
+++ 007.js
@@ -0,0 +1,7 @@
+/*
+Create a variable called text that contains a sentence. Use the replace() method to replace a word in the sentence and display the result.
+*/
+
+let text = 'the green color will be replace'
+let newtext = text.replace('green', 'blue')
+console.log(newtext)
