0d1e8827486aad3d1c1c7530874eb102904636e8
Gabriel dos Santos <ogabrielmclmg@gmail.com>
2025-03-03 16:27:59 -0300

    exercises

---
+++ 023.js
@@ -0,0 +1,10 @@
+/*
+Create an object that represents a person and display its properties.
+*/
+
+const person = {
+    name: "John",
+    age: 25,
+    profession: "Engineer"
+}
+console.log(person)
