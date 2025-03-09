ee87812b5a9d116d4f30eefa7a45081b4835a46b
Gabriel dos Santos <ogabrielmclmg@gmail.com>
2025-03-09 19:14:38 -0300

    exercises

---
+++ 029.js
@@ -0,0 +1,10 @@
+/*
+Create a function that multiplies the numbers from 1 to 10 by a given number.
+*/
+
+function multiplyByN(n) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i * n)
+    }
+}
+multiplyByN(3)
