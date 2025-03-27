290b49cda7fcb28ccdf3644c3370f08de0d328b9
Gabriel dos Santos <ogabrielmclmg@gmail.com>
2025-03-27 17:28:29 -0300

    Create 038.js

---
+++ 038.js
@@ -0,0 +1,43 @@
+// Months Function: Write a function that converts a number (1-12) into its respective month name.
+
+function returMonth(n) {
+    switch (n) {
+        case 1:
+            return 'january'
+            break
+        case 2:
+            return 'february'
+            break
+        case 3:
+            return 'march'
+            break
+        case 4:
+            return 'april'
+            break
+        case 5:
+            return 'may'
+            break
+        case 6:
+            return 'june'
+            break
+        case 7:
+            return 'july'
+            break
+        case 8:
+            return 'august'
+            break
+        case 9:
+            return 'september'
+            break
+        case 10:
+            return 'october'
+            break
+        case 11:
+            return 'november'
+            break
+        case 12:
+            return 'december'
+        default:
+            break
+    }
+}
